67ec07478c5e01c6932212ed8607765268200e03
Ella Jaravata <ella.jaravata@gmail.com>
2018-07-24 14:37:56 +0000

    automatically backed up by learn

--- mothers_day.rb
+++ mothers_day.rb
@@ -1,7 +1,7 @@
 ## Define your method, mothers_day, below. Go through the README and update your method as needed!
 
-def mothers_day(name="Mom")
-  puts "Happy Mother's Day, #{name}!<3"
+def mothers_day(<name>="Mom")
+  puts "Happy Mother's Day, #{<name>}!<3"
 end
 
 mothers_day
